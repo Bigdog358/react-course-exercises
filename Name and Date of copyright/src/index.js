5bc040d1bdb15843510d8fa428ee780998f0b2b7
Petr Kozeluh <140262675+Bigdog358@users.noreply.github.com>
2026-06-21 14:35:51 +0200

    Add files via upload

---
+++ Name and Date of copyright/src/index.js
@@ -0,0 +1,24 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const fName = "Petr";
+const lName = "Koželuh";
+const currentYear = new Date().getFullYear();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <p>
+      Created by {fName} {lName}.
+    </p>
+    <p>Copyright {currentYear}.</p>
+  </div>
+);
