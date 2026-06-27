b5c71169e315a5eea11121585f066db0a91f79ca
Petr Kozeluh <140262675+Bigdog358@users.noreply.github.com>
2026-06-27 20:57:19 +0200

    Add files via upload

---
+++ React components/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import Heading from "./Heading";
+//import List from "./List";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //<div>
+  //<Heading />
+  //<List />
+  //</div>
+  <App />
+);
