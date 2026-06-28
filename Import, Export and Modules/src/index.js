94431932c0f4b7671ebf6623851e7f6b8a4a5c5e
Petr Kozeluh <140262675+Bigdog358@users.noreply.github.com>
2026-06-28 18:06:59 +0200

    Add files via upload

---
+++ Import, Export and Modules/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PI, { doublePi, triplePi } from "./math.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ul>
+    <li>{PI}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>
+);
